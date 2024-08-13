d601d8e887f2198148e6ed50197ec70d15f57af9
Dmitry <manyakindima@gmail.com>
2024-08-13 13:34:41 +0300

    theme configuration

--- app/src/main/java/com/radlance/mymoscow/MainActivity.kt
+++ app/src/main/java/com/radlance/mymoscow/MainActivity.kt
@@ -11,14 +11,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.radlance.mymoscow.ui.theme.MyMoscowTheme
+import com.radlance.mymoscow.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyMoscowTheme {
+            AppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    MyMoscowTheme {
+    AppTheme {
         Greeting("Android")
     }
 }
