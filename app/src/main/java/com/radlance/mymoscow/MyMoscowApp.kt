bf0733630cbb768a0056df62c810722119122e87
Dmitry <manyakindima@gmail.com>
2024-08-13 14:01:55 +0300

    MyMoscowTopAppBar

---
+++ app/src/main/java/com/radlance/mymoscow/MyMoscowApp.kt
@@ -0,0 +1,9 @@
+package com.radlance.mymoscow
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyMoscow(modifier: Modifier = Modifier) {
+
+}
---
+++ app/src/main/java/com/radlance/mymoscow/MyMoscowTopAppBar.kt
@@ -0,0 +1,49 @@
+package com.radlance.mymoscow
+
+import androidx.annotation.StringRes
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+
+
+enum class MyMoscowScreen(@StringRes val title: Int) {
+    Start(R.string.separated_app_name)
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyMoscowTopAppBar(
+    canNavigateBack: Boolean,
+    currentScreen: MyMoscowScreen,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(text = stringResource(id = currentScreen.title)) },
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(id = R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+private fun MyMoscowTopAppBarPreview() {
+    MyMoscowTopAppBar(canNavigateBack = true, currentScreen = MyMoscowScreen.Start, navigateUp = {})
+}
