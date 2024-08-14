adf20c5c48893979e0957bbadb2c0dd4adb40ebf
Dmitry <manyakindima@gmail.com>
2024-08-14 16:12:41 +0300

    places description

--- app/src/main/java/com/radlance/mymoscow/domain/Recommendation.kt
+++ app/src/main/java/com/radlance/mymoscow/domain/Recommendation.kt
@@ -6,6 +6,7 @@ import androidx.annotation.StringRes
 data class Recommendation(
     val id: Int,
     @StringRes val titleResourceId: Int,
+    @StringRes val description: Int,
     @DrawableRes val imageResourceId: Int,
     val categoryId: Int
 )
